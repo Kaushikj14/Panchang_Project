0b70f9935c451c4486dd46874e1845610a3f0a0b
Kaushikj14 <kaushikjain1423@gmail.com>
2024-10-08 01:44:13 +0530

    card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './Components/Navbar/Header'
 import Footer from './Components/Footer/Footer'
+import Card from './Components/Card/Card'
 
 
 function App() {
@@ -9,7 +10,13 @@ function App() {
   return (
     <>
       <Header />
-      <div className='bg-indigo-600 w-full h-screen'></div>
+      <div className='bg-indigo-600 w-full h-screen'>
+      
+        <div className="flex justify-center items-center h-full">
+          <Card />
+        </div>
+      </div>
+      
       <Footer />
     </>
   )
